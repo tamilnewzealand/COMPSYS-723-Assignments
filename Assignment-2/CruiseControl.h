9e6ecf3396133a76b5b994c4f33c6a2e71baefb9
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2018-05-26 17:05:29 +1200

    Rename and add more constants

---
+++ Assignment-2/CruiseControl.h
@@ -0,0 +1,9 @@
+#define PedalsMin 3.00
+#define SpeedMin 30.00
+#define SpeedMax 150.00
+
+#define SpeedMini 32.500
+#define SpeedMaxi 147.50
+#define SpeedInc 2.50
+
+#define Zero 0.00
--- Assignment-2/defines.h
+++
@@ -1,3 +0,0 @@
-#define PedalsMin 3.00
-#define SpeedMin 30.00
-#define SpeedMax 150.00
